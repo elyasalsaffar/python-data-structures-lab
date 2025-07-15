25279f6a03dea4a753c3fb387df39f8c4e89148e
elyasalsaffar <elyas.dusty@gmail.com>
2025-07-15 20:16:12 +0300

    Completed Level Up

--- exercises.py
+++ exercises.py
@@ -117,3 +117,39 @@ def list_home_town_items():
 # Call the function and print the result
 # print('Exercise 5:', list_home_town_items())
 # list_home_town_items()
+
+
+
+# Level Up
+
+# Exercise 6: Celebrate Students
+#
+# Using the list of students and a list comprehension, assign to a variable named awesome_students a new list containing strings.
+# For example: ["Tina is awesome!", "Fred is awesome!", "Wilma is awesome!"]
+
+def create_awesome_students():
+    # your code here
+    students = ['Elyas', 'Ali', 'Sayed', 'Mohammed', 'Issam']
+    awesome_student = [f'{student} is awesome!' for student  in students]
+    print(awesome_student)
+
+# Call the function and print the result
+# print('Exercise 6:', create_awesome_students())
+# create_awesome_students()
+
+
+
+# Exercise 7: Filter Foods
+#
+# Assign to a variable named foods_with_an_a the result of list comprehension that filters the foods tuple to only include food strings that contain the letter 'a'.
+# For example, if foods is a tuple of ('Taco', 'Burrito', 'Sandwich'), foods_with_an_a would be a list equal to ['Taco', 'Sandwich']
+
+def filter_foods_with_a():
+    # your code here
+    foods = ('Taco', 'Burrito', 'Sandwich')
+    foods_with_an_a = list(filter(lambda food : 'a' in food, foods))
+    print(foods_with_an_a)
+
+# Call the function and print the result
+# print('Exercise 7:', filter_foods_with_a())
+# filter_foods_with_a()
